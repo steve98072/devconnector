70f6f1a6be7bd03ffa545bb94f3948df9ada696a
Steve Zhu <redwood98072@gmail.com>
2019-03-14 20:40:59 -0700

    able to create profile

---
+++ client/src/components/common/TextAreaFieldGroup.js
@@ -0,0 +1,32 @@
+import React from "react";
+import classnames from "classnames";
+import propTypes from "prop-types";
+
+const TextAreaField = ({ name, placeholder, value, error, info, onChange }) => {
+  return (
+    <div className="form-group">
+      <textarea
+        className={classnames("form-control form-control-lg", {
+          "is-invalid": error
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+      {info && <small className="form-text text-muted">{info}</small>}
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+TextAreaField.propTypes = {
+  name: propTypes.string.isRequired,
+  placeholder: propTypes.string,
+  value: propTypes.string.isRequired,
+  info: propTypes.string,
+  error: propTypes.string,
+  onChange: propTypes.func.isRequired
+};
+
+export default TextAreaField;
