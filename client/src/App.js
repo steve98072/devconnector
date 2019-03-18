0cdeb50d41bc2be7f3541cb78887524d48bcc6fc
Steve Zhu <redwood98072@gmail.com>
2019-03-17 22:16:20 -0700

    added prod config file

--- client/src/App.js
+++ client/src/App.js
@@ -23,9 +23,11 @@ import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from './components/profile/Profile';
 import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
 
 import "./App.css";
 import { clearCurrentProfile } from "./actions/profileActions";
+
 
 
 // Check for token, make sure that no matter where to go, the redux has the data for user
@@ -94,6 +96,13 @@ class App extends Component {
                   exact
                   path="/add-education"
                   component={AddEducation}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/feed"
+                  component={Posts}
                 />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+// make object available
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
+}
